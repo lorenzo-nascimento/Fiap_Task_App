48b193a51e4272c5481fe9999a0f284cbac03de6
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-08 11:43:43 -0300

    Implementando o Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:todo_app/pages/home/home_page.dart';
+import 'package:todo_app/provider/task_group_provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    url: '',
-    anonKey: '',
+    url: 'https://wvzossutukrwdduwnpmj.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind2em9zc3V0dWtyd2RkdXducG1qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjgzOTU2MTUsImV4cCI6MjA0Mzk3MTYxNX0.vpQK9cf-DQGGC0bGMva1U6g8D_V7A81YvHUePRyM3qg',
   );
 
-  runApp(const MyApp());
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(
+          create: (context) => TaskGroupProvider()..listTaskGroups())
+    ],
+    child: const MyApp(),
+  )); // Multiprovider
 }
 
 class MyApp extends StatelessWidget {
